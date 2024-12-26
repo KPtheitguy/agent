fff2ce538e6669f14d5de7f84800a9c7b64c6dc8
Koushik Polsana <Support@digitycs.com>
2024-12-26 09:50:26 -0700

    sync

--- main.py
+++
@@ -1,6 +0,0 @@
-from agent.core.agent_portal import AgentPortal
-
-if __name__ == "__main__":
-    portal = AgentPortal()
-    # Example values for demonstration
-    portal.register_agent("dummy-token", "dummy-agent")
